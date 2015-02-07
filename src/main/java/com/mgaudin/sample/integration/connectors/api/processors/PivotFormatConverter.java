0372a9dcf7eaed3039abc24e5bb9d150d87ad024
Maxime Gaudin <gaudin.maxime@gmail.com>
2015-02-07 20:52:10 +0100

    Minor watchdog before further work

--- src/main/java/com/mgaudin/sample/integration/connectors/api/processors/PivotFormatConverter.java
+++ src/main/java/com/mgaudin/sample/integration/connectors/api/processors/PivotFormatConverter.java
@@ -2,17 +2,13 @@ package com.mgaudin.sample.integration.connectors.api.processors;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.component.restlet.RestletConstants;
-import org.restlet.Response;
-import org.restlet.data.Status;
 import org.springframework.stereotype.Service;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @Service
 public class PivotFormatConverter implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
-        Response response = exchange.getIn().getHeader(RestletConstants.RESTLET_RESPONSE, Response.class);
-        response.setStatus(Status.CLIENT_ERROR_NOT_FOUND);
-        exchange.getOut().setBody(response);
+        throw new NotImplementedException();
     }
 }
